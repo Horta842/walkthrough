1cd1890c979352418d69fa710afda817ab5dbf50
rafaelhv <horta842@gmail.com>
2021-11-03 12:19:50 -0600

    paso 15 Anidamiento de una vista en otra

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,26 +1,10 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
+	"sap/ui/core/mvc/Controller"
 ], function(
-	Controller,
-    MessageToast,
-    JSONModel,
-    ResourceModel
+	Controller
 ) {
 	"use strict";
 
 	return Controller.extend("walkthrough.controller.App", {
-       
-         
-        onShowHello : function () {
-             // read msg from i18n model
-            var oBundle = this.getView().getModel("i18n").getResourceBundle();
-            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-            // show message
-            MessageToast.show(sMsg);
-         }
 	});
 });
