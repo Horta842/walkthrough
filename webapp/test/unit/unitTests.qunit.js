b05ea715a0d91f14280f80f5a98ef946875671a6
rafaelhv <horta842@gmail.com>
2021-11-05 11:17:04 -0600

    Paso 27 cargando datos del mockdata desde servidor y cambiando la estructura del json para referenciar

--- webapp/test/unit/unitTests.qunit.js
+++ webapp/test/unit/unitTests.qunit.js
@@ -1,11 +1,12 @@
 /* global QUnit */
+
 QUnit.config.autostart = false;
 
 sap.ui.getCore().attachInit(function () {
 	"use strict";
 
 	sap.ui.require([
-		"walkthrough/test/unit/AllTests"
+		"walkthrough/test/unit/model/formatter"
 	], function () {
 		QUnit.start();
 	});
